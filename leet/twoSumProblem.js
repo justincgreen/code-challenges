311c6ea8c0495a99384d2bfb67f0387bc2cc34ce
Justin Green <justingreendev@gmail.com>
2024-09-20 10:50:31 -0500

    Add twosum problem

---
+++ leet/twoSumProblem.js
@@ -0,0 +1,20 @@
+/*
+  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+  
+  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+  
+  You can return the answer in any order.
+*/
+
+const twoSum = (nums, target) => {
+  
+}
+
+// Test case 1
+twoSum([2,7,11,15], 9); // expected output [0,1]
+
+// Test case 2
+twoSum([3,2,4], 6); // expected output [1,2]
+
+// Test case 3
+twoSum([3,3], 6); // expected output [0,1]
