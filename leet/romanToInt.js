f4f13cb5521f3b979516bca57250ac19365e18ae
Justin Green <justingreendev@gmail.com>
2024-09-20 15:15:30 -0500

    roman to Int

---
+++ leet/romanToInt.js
@@ -0,0 +1,20 @@
+// WIP
+const romanToInt = (s) => {
+  const map = new Map();
+  map.set('I', 1);
+  map.set('V', 5);
+  map.set('X', 10);
+  map.set('L', 50);
+  map.set('C', 100);
+  map.set('D', 500);
+  map.set('M', 1000);
+  // console.log(map.size);
+  
+  for(let i = 0; i < map.size; i++) {
+    console.log(map[s]);
+  }
+  
+  console.log(map.get(s));
+}
+
+romanToInt('I');
